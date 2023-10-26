3a5fdb2e900e532f5fcecae0f70900c51a129286
Danilo Souza <lcd.souza44@gmail.com>
2023-10-26 15:05:19 -0300

    commit teste

--- envia.php
+++ envia.php
@@ -1,20 +1,23 @@
 <?php
-
-    $nome = addcslashes($_POST['nome']);
-    $email = addcslashes($_POST['email']);
-    $telefone = addcslashes($_POST['telefone']);
-    $mensagem = addcslashes($_POST['mensagem']);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+    $mensagem = $_POST['mensagem'];
 
     $para = "danilosrsilvarr@gmail.com";
     $assunto = "Dúvidas - Danilo Souza";
 
     $corpo =  "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Telefone: ".$telefone."\n"."Mensagem: ".$mensagem;
- 
-    $cabeca = "From: ".$nome."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
 
-    if(mail($para,$assunto,$corpo,$cabeca)){
-        echo("E-mail enviado com sucesso!");
-    }else{
-        echo("Houve um erro ao enviar o e-mail!");
+    $cabecalho = "From: ".$nome."<".$email.">"."\r\n".
+                 "Reply-To: ".$email."\r\n".
+                 "X-Mailer: PHP/".phpversion();
+
+    if(mail($para, $assunto, $corpo, $cabecalho)){
+        echo "E-mail enviado com sucesso!";
+    } else {
+        echo "Houve um erro ao enviar o e-mail!";
     }
+}
 ?>
